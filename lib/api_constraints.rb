49ffb3bff2b979d4e35bf4025940d865206d20e0
leohc87 <leohc@live.nl>
2026-05-26 23:00:25 -0300

    Adiciona constraints and manager to version api

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # frozen_string_literal: true
 
 class ApiConstraints
@@ -12,3 +13,17 @@ class ApiConstraints
     @default || req.headers[:Accept]&.include?("application/vnd.marketplace.v#{@version}")
   end
 end
+=======
+class ApiConstraints
+  attr_accessor :version, :default
+
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+  @default || req.headers[:Accept]&.include?("application/vnd.marketplace.v#{@version}")
+end
+end
+>>>>>>> 3d51741 (Adiciona constraints and manager to version api)
