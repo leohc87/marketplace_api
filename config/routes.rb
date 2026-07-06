3c1844c4e447c98207707fb9b487f5f8a0627a0a
Leandro Augusto <leohc@live.nl>
2026-07-06 14:01:54 -0300

    feat: add API v1 users index route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: :json }, path: '/' do
       scope module: :v1,
             constraints: ApiConstraints.new(version: 1, default: true) do
-        # suas rotas aqui
+        resources :users, only: %i[index show create]
       end
     end
   end
