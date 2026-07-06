f041076b569d013e6da972fba0d6e7b77b5089eb
Leandro Augusto <leohc@live.nl>
2026-07-06 12:49:29 -0300

    feat: add User model with validations and API endpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_secure_password
+
+  validates :email, presence: true
+  validates :password_digest, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.build(:user) }
 
-  subject { user } 
+  subject { user }
 
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password_digest) }
+  end
 end
