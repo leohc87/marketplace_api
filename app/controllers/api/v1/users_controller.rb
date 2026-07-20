5b24f09c1fdca5747180527bdf029386139a37ed
Leandro Augusto <leohc@live.nl>
2026-07-20 11:27:09 -0300

    Adiciona metodos de usuarios controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
-
 module Api
   module V1
     class UsersController < ApplicationController
-      def index
-        users = User.all
-        render json: users, status: :ok
+      respond_to :json
+
+      def show
+        respond_with User.find(params[:id])
       end
     end
   end
