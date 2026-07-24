9c5c84092a8ec94b1534aa1a7b0c65bfed68c8d3
Leandro Augusto <leohc@live.nl>
2026-07-24 10:51:39 -0300

    Adicionando o susário create add point

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
+Rails.application.routes.draw do # rubocop:disable Lint/Syntax
   devise_for :users
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: :json }, path: '/' do
       scope module: :v1,
             constraints: ApiConstraints.new(version: 1, default: true) do
-        resources :users, only: %i[index show create]
+        resources :users, only: [:create, :show]
       end
     end
-  end
+  end # rubocop:disable Lint/Syntax
 end
