77f9561d028e40aaa6d780dfa8ccfcc0098df147
Leandro Augusto <leohc@live.nl>
2026-07-27 15:09:20 -0300

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,32 @@ module Api
       def show
         respond_with User.find(params[:id])
       end
+
+      def create
+        user = User.create(user_params)
+
+        if user.save
+          render json: user, status: :created, location: [:api, user]
+        else
+          render json: { errors: user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        user = User.find(params[:id])
+
+        if user.update(user_params)
+          render json: user, status: :ok, location: [:api, user]
+        else
+          render json: { errors: user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -55,4 +55,44 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
     end
   end
+
+  describe 'PUT/PATCH #update' do
+    context 'when is successfully updated' do
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        patch :update, params: { id: @user.id, user: { email: 'newemail@example.com' } }, format: :json
+      end
+
+      it 'updates the user email' do
+        @user.reload
+        expect(@user.email).to eq 'newemail@example.com'
+      end
+
+      it 'renders the json representation for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq 'newemail@example.com'
+      end
+
+      it { should respond_with :ok }
+    end
+
+    context 'when is not updated' do
+      before(:each) do
+        @user = FactoryBot.create(:user) # criando novo registro no banco de dados para teste
+        patch :update, params: { id: @user.id, user: { email: 'bademail.com' } }, format: :json # fazendo req via patch
+      end
+
+      it 'renders an errors json' do # entra aqui pra executar o teste, e espera que retorne um json com a chave errors
+        user_response = JSON.parse(response.body, symbolize_names: true) # response.body é o corpo da resposta da requisição, que é um json, e o parse transforma em hash
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on why the user could not be updated' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include 'is invalid'
+      end
+
+      it { should respond_with :unprocessable_entity }
+    end
+  end
 end
