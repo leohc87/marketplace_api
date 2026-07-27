ac205cf85b3b0bb4c788665406efaba6946da402
Leandro Augusto <leohc@live.nl>
2026-07-27 10:15:59 -0300

    Refactory and change rubocop code style

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
-  constraints subdomain: 'api' do
-    namespace :api, defaults: { format: :json }, path: '/' do
-      scope module: :v1,
-            constraints: ApiConstraints.new(version: 1, default: true) do
-        resources :users, only: %i[index show create]
-      end
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:show, :create]
     end
   end
 end
